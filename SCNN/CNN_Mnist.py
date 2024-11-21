f197b114f5f86b9c9c70c5d3f829c95394450efd
wangluchen567 <wangluchen567@qq.com>
2024-11-21 16:27:57 +0800

    加入保存参数功能

--- SCNN/CNN_Mnist.py
+++ SCNN/CNN_Mnist.py
@@ -4,8 +4,9 @@ import gzip
 import pickle
 import numpy as np
 
-from Core.Optimizer import Adam
+from tqdm import tqdm
 from Core.Module import LeNet5
+from Core.Optimizer import Adam
 from Core.Loss import CrossEntropyWithSoftmax
 
 
@@ -25,7 +26,7 @@ def train_epoch(model, optimizer, X, Y, batch_size):
     """训练一个epoch"""
     model.train()
     train_loss = 0
-    for i in np.arange(0, len(X), batch_size):
+    for i in tqdm(np.arange(0, len(X), batch_size)):
         # 需要将input形状调整为(batch_size, in_channels, height, width)(NCHW格式)
         input_ = X[i:i + batch_size].reshape(-1, 1, 28, 28).transpose(0, 1, 3, 2)
         truth = Y[i:i + batch_size]
@@ -39,7 +40,7 @@ def train_epoch(model, optimizer, X, Y, batch_size):
     return model, optimizer, train_loss
 
 
-def train_model(model, train_data, train_label, valid_data, valid_label, num_epochs=30):
+def train_model(model, train_data, train_label, valid_data, valid_label, num_epochs=20, save=True):
     """训练模型"""
     batch_size = 64
     optimizer = Adam(model=model, learning_rate=1.e-3)
@@ -49,6 +50,11 @@ def train_model(model, train_data, train_label, valid_data, valid_label, num_epo
         accuracy = valid_model(model, valid_data, valid_label)
         print("epoch: [{:d}/{:d}], loss: {:.3f}, accuracy: {:.3f}, time(s): {:.3f}".
               format(epoch + 1, num_epochs, train_loss, accuracy, time.time() - start))
+        if save:
+            # 保存每个epoch的模型参数
+            params_dict = model.get_parameters()
+            with open('LeNet-5_Params_' + str(epoch + 1) + '.json', 'w') as f:
+                json.dump(params_dict, f)
     return model
 
 
@@ -67,14 +73,18 @@ if __name__ == '__main__':
     x_train, y_train, x_valid, y_valid = load_data(data_path)
     y_train = y_train.reshape(-1, 1)
     y_valid = y_valid.reshape(-1, 1)
-    train_size = 10000
+    train_size = 10000  # 若要全部训练需要设置为50k
     valid_size = 1000
     # 创建模型
     model = LeNet5()
-    model = train_model(model, x_train[:train_size], y_train[:train_size], x_valid[:valid_size], y_valid[:valid_size])
+    start = time.time()
+    model = train_model(model, x_train[:train_size], y_train[:train_size],
+                        x_valid[:valid_size], y_valid[:valid_size], num_epochs=20, save=False)
+    train_time = time.time() - start
     accuracy = valid_model(model, x_valid, y_valid)
-    print("full dataset accuracy: {:.3f} %".format(accuracy * 100))
-    # 保存模型参数
+    print("Test accuracy: {:.3f} %".format(accuracy * 100))
+    print("running time: {:.3f}".format(train_time))
+    # 保存每个epoch的模型参数
     params_dict = model.get_parameters()
-    with open('LeNet-5_Params.json', 'w') as f:
+    with open('LeNet-5_Params_.json', 'w') as f:
         json.dump(params_dict, f)
